6911ac4b93c30eaa5a702f9a712e5ab297dc8bb8
Piotr Srebniak <piotr.srebniak@gmail.com>
2019-11-10 10:37:43 +0100

    fix(#10): remove not necessary files added during merge

--- swagger-typescript-client-generator-runtime/lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-export interface ApiResponse<T> extends Response {
-    json(): Promise<T>;
-}
-export declare type RequestFactoryType = (path: string, query: any, body: any, formData: any, headers: any, method: string, configuration: any) => Promise<ApiResponse<any>>;
