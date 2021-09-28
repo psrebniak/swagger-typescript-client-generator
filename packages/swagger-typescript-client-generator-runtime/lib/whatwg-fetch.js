0ceec76b2628dce3b4d02829e4ca3e2a7fbe73fa
Piotr Srebniak <piotr.srebniak@gmail.com>
2021-09-28 17:51:25 +0200

    feat: rebuild

--- packages/swagger-typescript-client-generator-runtime/lib/whatwg-fetch.js
+++ packages/swagger-typescript-client-generator-runtime/lib/whatwg-fetch.js
@@ -2,38 +2,42 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.WhatWgFetchRequestFactory = void 0;
 var serialize_1 = require("./serialize");
-var WhatWgFetchRequestFactory = function (baseUrl, options) { return function (path, query, body, formData, headers, method, configuration) {
-    var headersObject = new Headers(options.requestInit.headers || {});
-    new Headers(headers).forEach(function (value, key) {
-        headersObject.set(key, String(value));
-    });
-    var fetchOptions = Object.assign({}, options.requestInit, {
-        method: method,
-        headers: headersObject
-    });
-    if (body && typeof body === "string") {
-        fetchOptions.body = body;
-    }
-    else if (body && typeof body === "object" && Object.keys(body).length > 0) {
-        fetchOptions.body = JSON.stringify(body);
-    }
-    else if (formData && Object.keys(formData).length > 0) {
-        fetchOptions.body = Object.keys(formData).reduce(function (data, key) {
-            data.append(key, formData[key]);
-            return data;
-        }, new FormData());
-    }
-    else if (formData) {
-        fetchOptions.body = formData;
-    }
-    var hasQuery = query && Object.keys(query).length > 0;
-    var fullUrl = [
-        baseUrl,
-        path,
-        hasQuery ? (path.includes("?") ? "&" : "?") : "",
-        hasQuery ? serialize_1.serialize(query) : ""
-    ].join("");
-    var callback = typeof options.fetch === "function" ? options.fetch : fetch;
-    return callback(fullUrl, fetchOptions);
-}; };
+var WhatWgFetchRequestFactory = function (baseUrl, options) {
+    return function (path, query, body, formData, headers, method, configuration) {
+        var headersObject = new Headers(options.requestInit.headers || {});
+        new Headers(headers).forEach(function (value, key) {
+            headersObject.set(key, String(value));
+        });
+        var fetchOptions = Object.assign({}, options.requestInit, {
+            method: method,
+            headers: headersObject,
+        });
+        if (body && typeof body === "string") {
+            fetchOptions.body = body;
+        }
+        else if (body &&
+            typeof body === "object" &&
+            Object.keys(body).length > 0) {
+            fetchOptions.body = JSON.stringify(body);
+        }
+        else if (formData && Object.keys(formData).length > 0) {
+            fetchOptions.body = Object.keys(formData).reduce(function (data, key) {
+                data.append(key, formData[key]);
+                return data;
+            }, new FormData());
+        }
+        else if (formData) {
+            fetchOptions.body = formData;
+        }
+        var hasQuery = query && Object.keys(query).length > 0;
+        var fullUrl = [
+            baseUrl,
+            path,
+            hasQuery ? (path.includes("?") ? "&" : "?") : "",
+            hasQuery ? serialize_1.serialize(query) : "",
+        ].join("");
+        var callback = typeof options.fetch === "function" ? options.fetch : fetch;
+        return callback(fullUrl, fetchOptions);
+    };
+};
 exports.WhatWgFetchRequestFactory = WhatWgFetchRequestFactory;
